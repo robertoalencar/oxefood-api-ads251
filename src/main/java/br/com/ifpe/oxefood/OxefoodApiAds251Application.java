dffbc2be06ffd8771ec8356ebb0b598d43ab336b
Roberto Alencar <rroberto.alencar@gmail.com>
2025-04-03 20:50:18 -0300

    Enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiAds251Application.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiAds251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiAds251Application.class, args);
+	}
+
+}
